6896bef3701e384969dfa7701e8ea048b70d2af4
AplhaA <133354992+AplhaA@users.noreply.github.com>
2023-05-13 20:55:40 +0800

    3rdNew

--- app/src/main/java/com/example/ehotline/MainActivity.java
+++ app/src/main/java/com/example/ehotline/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ehotline;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+               Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+               startActivity(intent);
+
+
+            }
+        });
+
+
+
+
     }
 }
